9d76e063e3970d9c1fcd871959d7e3a8501a0bad
experiencei <ayelojahighbee01@gmail.com>
2021-10-16 15:49:57 +0100

    working on uploading images and caption

--- src/components/ImageUpload.js/ImageUpload.js
+++ src/components/ImageUpload.js/ImageUpload.js
@@ -3,7 +3,12 @@ import { Button } from '@material-ui/core';
 
 
 function ImageUpload() {
-  const [caption, setCaption] = useState(initialState)
+  const [caption, setCaption] = useState('')
+  const [progress, setProgress] = useState(0)
+  const [image, setImage] = useState(null);
+
+  
+
     return (
         <div>
         <input type="text" placeholder="Enter a caption..." onChange={event => setCaption(event.target.value)} value={caption}/>
