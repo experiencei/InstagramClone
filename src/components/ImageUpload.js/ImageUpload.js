e2993b2544b7cc9bd51cbcdb0cbb166c7eb101f0
experiencei <ayelojahighbee01@gmail.com>
2021-10-16 16:02:22 +0100

    working on uploading images and caption

--- src/components/ImageUpload.js/ImageUpload.js
+++ src/components/ImageUpload.js/ImageUpload.js
@@ -13,8 +13,17 @@ function ImageUpload() {
         setImage(e.target.files[0])
       }
   }
-  const handleUpload = (e) => {
-     
+
+  const handleUpload = () => {
+     const uploadTask = storage.ref(`images/${image.name}`).put(image)
+     uploadTask.on("state_changed" , 
+     (snapshot) => {
+         const progress = Math.round(
+             (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+         );
+         setProgress(progress)
+     }
+     )
   }
 
     return (
